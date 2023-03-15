3cf3a816c76ab436a2a16f6de1f078c3a4842d91
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-03-15 15:01:29 -0300

    Exporta funciones de analisis a archivo auxiliar

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
+from functools import reduce
+
 import funciones_auxiliares_hito_2 as aux2
 
 RUTA_2013 = 'input/Egresos_Hospitalarios_2013/Egresos_Hospitalarios_2013.csv'
@@ -166,3 +168,87 @@ def obtener_diccionario_cie():
     cie = tratar_columna_codigo(cie)
 
     return cie
+
+
+def aplanar_columnas(df):
+    tmp = df.copy()
+
+    tmp.columns = ['_'.join(col).strip('_') for col in tmp.columns]
+
+    return tmp
+
+
+def obtener_diagnosticos_hospital(df, glosa):
+    diagnosticos_hospital = df.query('GLOSA_ESTABLECIMIENTO_SALUD == @glosa')
+
+    return diagnosticos_hospital['DIAG1'].unique()
+
+
+def calcular_porcentaje_metrica_por_diagnostico(df, subgrupo_ranking, variable_a_analizar):
+    return df[variable_a_analizar] / df.groupby(subgrupo_ranking)[variable_a_analizar].transform('sum')
+
+
+def obtener_ranking_total(df, agrupar_por, subgrupo_ranking, variable_a_analizar):
+    tmp = df.copy()
+
+    df_agrupada = tmp.groupby(agrupar_por).agg(N_Egresos=('TIPO_EDAD', 'sum'),
+                                               DIAS_ESTADA_Promedio=('DIAS_ESTADA', 'mean'))
+
+    orden_ranking = subgrupo_ranking + variable_a_analizar
+    df_agrupada = df_agrupada.sort_values(orden_ranking, ascending=False)
+
+    df_agrupada['%_Egresos'] = calcular_porcentaje_metrica_por_diagnostico(
+        df_agrupada, subgrupo_ranking, 'N_Egresos')
+
+    df_agrupada['%_DIAS_ESTADA_Promedio'] = calcular_porcentaje_metrica_por_diagnostico(
+        df_agrupada, subgrupo_ranking, 'DIAS_ESTADA_Promedio')
+
+    orden_columnas = ['N_Egresos', '%_Egresos', 'DIAS_ESTADA_Promedio', '%_DIAS_ESTADA_Promedio']
+
+    df_agrupada = df_agrupada[orden_columnas]
+
+    df_agrupada = df_agrupada.reset_index()
+
+    return df_agrupada
+
+
+def obtener_tabla_posicion_hospital_en_ranking(ranking, glosa_hospital, subgrupo_ranking):
+    tmp = ranking.copy()
+
+    tmp['Ranking'] = (tmp.groupby(subgrupo_ranking).cumcount()) + 1
+    tmp = tmp.query('GLOSA_ESTABLECIMIENTO_SALUD == @glosa_hospital')
+    tmp = tmp.sort_values('Ranking').reset_index(drop=True)
+
+    return tmp
+
+
+def analizar_ranking_hospital(df, hospital_a_analizar, agrupar_por, subgrupo_ranking, variable_a_analizar):
+    tabla_ranking_global = obtener_ranking_total(
+        df, agrupar_por, subgrupo_ranking, variable_a_analizar)
+    tabla_posicion_hospital = obtener_tabla_posicion_hospital_en_ranking(tabla_ranking_global,
+                                                                         hospital_a_analizar,
+                                                                         subgrupo_ranking)
+
+    return tabla_ranking_global, tabla_posicion_hospital
+
+
+def agregar_sufijo_especifico(dfs, sufijos):
+    tmps = dfs.copy()
+
+    for i in range(len(tmps)):
+        tmps[i] = tmps[i].add_suffix(sufijos[i])
+
+    return tmps
+
+
+def unir_tablas_de_posicion_por_diagnostico(diccionario_tablas_posicion, unir_en):
+    dfs_con_indice = list(map(lambda x: x.set_index(unir_en), diccionario_tablas_posicion.values()))
+    dfs_con_sufijo = agregar_sufijo_especifico(dfs_con_indice,
+                                               list(diccionario_tablas_posicion.keys()))
+
+    unidas = reduce(lambda left, right: pd.merge(left, right, left_index=True, right_index=True,
+                                                 how='outer'), dfs_con_sufijo)
+
+    unidas = unidas.reset_index()
+
+    return unidas
