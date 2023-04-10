60cc60a863ba31beea035e12d5b32abc2f71e6e4
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-04-10 11:51:45 -0400

    Exporta correctamente las regiones sin prob encod

--- funciones_auxiliares_metricas_egresos.py
+++ funciones_auxiliares_metricas_egresos.py
@@ -7,8 +7,8 @@ from functools import reduce
 RUTA_CIE = 'input/CIE-10.xlsx'
 
 
-LEER_ANIO_INICIO = 2014
-LEER_ANIO_FINAL = 2014
+LEER_ANIO_INICIO = 2001
+LEER_ANIO_FINAL = 2020
 ARCHIVOS_A_LEER = [f'input/Egresos_Hospitalarios_{i}.csv' for i in range(LEER_ANIO_INICIO,
                                                                          LEER_ANIO_FINAL + 1)]
 
