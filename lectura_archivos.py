189dd7aaa8f071ee79c078329da33e46b6cc0eed
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-04-13 14:11:05 -0400

    Modulariza lectura de archivos

---
+++ lectura_archivos.py
@@ -0,0 +1,69 @@
+import polars as pl
+
+DICT_VARIABLES = {
+    "ESTABLECIMIENTO_SALUD": pl.Int32,
+    "GLOSA_ESTABLECIMIENTO_SALUD": pl.Categorical,
+    "PERTENENCIA_ESTABLECIMIENTO_SALUD": pl.Categorical,
+    "SEREMI": pl.Int8,
+    "SERVICIO_DE_SALUD": pl.Int8,
+    "SEXO": pl.Int8,
+    "EDAD_CANT": pl.Int8,
+    "TIPO_EDAD": pl.Int8,
+    "EDAD_A_OS": pl.Int8,
+    "PUEBLO_ORIGINARIO": pl.Int8,
+    "PAIS_ORIGEN": pl.Int16,
+    "GLOSA_COMUNA_RESIDENCIA": pl.Categorical,
+    "REGION_RESIDENCIA": pl.Categorical,
+    "GLOSA_REGION_RESIDENCIA": pl.Categorical,
+    "PREVISION": pl.Int8,
+    "BENEFICIARIO": pl.Categorical,
+    "MODALIDAD": pl.Int8,
+    "PROCEDENCIA": pl.Int8,
+    "ANO_EGRESO": pl.Int16,
+    "FECHA_EGRESO": pl.Date,
+    "AREA_FUNCIONAL_EGRESO": pl.Int16,
+    "DIAS_ESTADA": pl.Int16,
+    "CONDICION_EGRESO": pl.Int8,
+    "INTERV_Q": pl.Int8,
+    "CODIGO_INTERV_Q_PPAL": pl.Int32,
+    "PROCED": pl.Categorical,
+    "CODIGO_PROCED_PPAL": pl.Categorical,
+    "GLOSA_PROCED_PPAL": pl.Categorical,
+}
+
+
+def remapear_int_q_y_muertes(df):
+    tmp = df.with_columns(
+        [
+            pl.col("INTERV_Q").map_dict({2: 0}, default=pl.col("INTERV_Q")).alias("INTERV_Q"),
+            pl.col("CONDICION_EGRESO")
+            .map_dict({1: 0, 2: 1}, default=pl.col("CONDICION_EGRESO"))
+            .alias("CONDICION_EGRESO"),
+        ]
+    )
+
+    return tmp
+
+
+def obtener_diagnosticos_unicos_de_hospital(df, hospital_a_analizar):
+    diags_hospital = (
+        df.filter(pl.col("ESTABLECIMIENTO_SALUD") == hospital_a_analizar)
+        .select(pl.col("DIAG1"))
+        .unique()
+    )
+
+    return diags_hospital
+
+
+def leer_archivos(filtro_hospital=11203):
+    with pl.StringCache():
+        df_nacional = pl.scan_csv("input/utf-8/*.csv", separator=";")
+        diags_torax = (
+            obtener_diagnosticos_unicos_de_hospital(df_nacional, filtro_hospital).collect(
+                streaming=True
+            )
+        ).to_series()
+        df = df_nacional.filter(pl.col("DIAG1").is_in(diags_torax))
+        df = remapear_int_q_y_muertes(df)
+
+        return df
