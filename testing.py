2d329e41968dde2602443bf379fbe5589dd29d65
MrHamburguesa <javierrojas14.jr@gmail.com>
2023-04-20 12:32:02 -0400

    Obtiene correctamente los tests

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 import unittest
-import pickle
+import calculo_metricas as aux
 
+import pickle
 import polars as pl
 
-from calculo_metricas import obtener_metricas_egresos
 
 with open(r"df_prueba.pickle", "rb") as file:
     DF_PRUEBA = pickle.load(file)
@@ -15,29 +15,31 @@ AGRUPACION = [
     "DIAG1",
 ]
 
+def testear_n_egresos(resultado_metricas, df_prueba, cod_hospital, diag):
+    resultado_filtro = df_prueba.filter(
+        (pl.col("ESTABLECIMIENTO_SALUD") == cod_hospital) & (pl.col("DIAG1") == diag)
+    ).shape[0]
+
+    resultado_metricas = (
+        resultado_metricas.filter(
+            (pl.col("ESTABLECIMIENTO_SALUD") == cod_hospital) & (pl.col("DIAG1") == diag)
+        )
+        .select("n_egresos")
+        .item()
+    )
+
+    return resultado_filtro == resultado_metricas
+
 
 class TestMetricasEgresos(unittest.TestCase):
     def testear_metricas(self):
-        resultados = obtener_metricas_egresos(DF_PRUEBA, AGRUPACION)
+        resultados = aux.obtener_metricas_egresos(DF_PRUEBA, AGRUPACION)
         filtros = [[112101, "P027"], [111195, "J189"], [103100, "P073"]]
 
         for cod_hospital, diag in filtros:
-            self.assertTrue(self.testear_n_egresos(resultados, DF_PRUEBA, cod_hospital, diag))
+            self.assertTrue(testear_n_egresos(resultados, DF_PRUEBA, cod_hospital, diag))
 
-    def testear_n_egresos(self, resultado_metricas, df_prueba, cod_hospital, diag):
-        resultado_filtro = df_prueba.filter(
-            (pl.col("ESTABLECIMIENTO_SALUD") == cod_hospital) & (pl.col("DIAG1") == diag)
-        ).shape[0]
 
-        resultado_metricas = (
-            resultado_metricas.filter(
-                (pl.col("ESTABLECIMIENTO_SALUD") == cod_hospital) & (pl.col("DIAG1") == diag)
-            )
-            .select("n_egresos")
-            .item()
-        )
-
-        return resultado_filtro == resultado_metricas
 
 
 if __name__ == "__main__":
